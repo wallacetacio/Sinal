8595b90ba4d9200673ffce9ba23a003337b65882
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-03 09:13:08 -0300

    Adiciona resolução ao exercício Sinal ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num;
+
+            Console.Write("Digite um número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if(num < 0 ){
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Negativo!");
+                Console.ResetColor();
+            }
+            else if(num == 0){
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Zero!");
+                Console.ResetColor();
+
+            }
+            else{
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Positivo!");
+                Console.ResetColor();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Sinal
-Exercício Sinal ETEC 2021
+## Exercício `Sinal` ETEC 2021
+
+Leia um número inteiro e exiba "Negativo", "Zero" ou "Positivo", conforme o caso.
+
+Ex.:
+```
+Digite um número: 35
+Positivo
+```
+ou
+```
+Digite um número: -10
+Negativo
+```
+ou
+```
+Digite um número: 0
+Zero
+```
